a1a96679c34c509305e8884b53cfdc9a1eda1b57
Ronald Chen <pyrogx1133@gmail.com>
2022-12-07 14:29:45 -0800

    implemented inorderIterator

--- lib/std/binary_tree.zig
+++ lib/std/binary_tree.zig
@@ -23,31 +23,66 @@ pub fn StaticBinaryTree(comptime T: type, comptime levels: u8) type {
             }
 
             pub fn parent(self: *Cursor) ?void {
+                if (self.parentIndex()) |index| {
+                    self.index = index;
+                } else {
+                    return null;
+                }
+            }
+
+            pub fn parentIndex(self: Cursor) ?usize {
                 if (self.index == 0) {
                     return null;
                 }
-                self.index = (self.index - 1) >> 1;
+                return (self.index - 1) >> 1;
             }
 
             pub fn leftChild(self: *Cursor) ?void {
+                if (self.leftIndex()) |index| {
+                    self.index = index;
+                } else {
+                    return null;
+                }
+            }
+
+            pub fn leftIndex(self: Cursor) ?usize {
                 if (self.index >= size / 2) {
                     return null;
                 }
-                self.index = (self.index << 1) + 1;
+                return (self.index << 1) + 1;
             }
 
             pub fn rightChild(self: *Cursor) ?void {
+                if (self.rightIndex()) |index| {
+                    self.index = index;
+                } else {
+                    return null;
+                }
+            }
+
+            pub fn rightIndex(self: Cursor) ?usize {
                 if (self.index >= size / 2) {
                     return null;
                 }
-                self.index = (self.index << 1) + 2;
+                return (self.index << 1) + 2;
             }
 
             pub fn sibling(self: *Cursor) ?void {
-                if (self.index & 1 == 0) {
+                if (self.siblingIndex()) |index| {
+                    self.index = index;
+                } else {
                     return null;
                 }
-                self.index += 1;
+            }
+
+            pub fn siblingIndex(self: Cursor) ?usize {
+                if (self.index == 0) {
+                    return null;
+                }
+                return if (self.index & 1 == 0)
+                    self.index - 1
+                else
+                    self.index + 1;
             }
         };
 
@@ -89,34 +124,80 @@ pub fn StaticBinaryTree(comptime T: type, comptime levels: u8) type {
                 },
             };
         }
+
+        pub const InorderIterator = struct {
+            cursor: Cursor,
+            done: bool = false,
+
+            pub fn next(it: *InorderIterator) ?T {
+                if (it.done) {
+                    return null;
+                }
+                const value = it.cursor.value();
+                if (it.cursor.rightChild() == null) {
+                    var last: ?usize = null;
+                    while (it.cursor.rightIndex() == last) {
+                        if (it.cursor.index == 0) {
+                            it.done = true;
+                            break;
+                        }
+                        last = it.cursor.index;
+                        _ = it.cursor.parent();
+                    }
+                } else {
+                    while (it.cursor.leftChild()) |_| {}
+                }
+                return value;
+            }
+
+            pub fn reset(it: *InorderIterator) void {
+                it.cursor.index = 0;
+                it.done = false;
+                while (it.cursor.leftChild()) |_| {}
+            }
+        };
+
+        pub fn inorderIterator(self: *Self) InorderIterator {
+            var it = InorderIterator{
+                .cursor = .{
+                    .tree = self,
+                },
+            };
+            it.reset();
+            return it;
+        }
     };
 }
 
 const TestData = struct {
-    level: u8,
-    order: []const u8,
+    levels: u8,
+    expected_order: []const u8,
 };
 
 test "preorderIterator" {
     inline for ([_]TestData{
         .{
-            .level = 1,
-            .order = &.{0},
+            .levels = 1,
+            .expected_order = &.{0},
         },
         .{
-            .level = 2,
-            .order = &.{ 0, 1, 2 },
+            .levels = 2,
+            .expected_order = &.{ 0, 1, 2 },
         },
         .{
-            .level = 3,
-            .order = &.{ 0, 1, 3, 4, 2, 5, 6 },
+            .levels = 3,
+            .expected_order = &.{ 0, 1, 3, 4, 2, 5, 6 },
         },
         .{
-            .level = 4,
-            .order = &.{ 0, 1, 3, 7, 8, 4, 9, 10, 2, 5, 11, 12, 6, 13, 14 },
+            .levels = 4,
+            .expected_order = &.{ 0, 1, 3, 7, 8, 4, 9, 10, 2, 5, 11, 12, 6, 13, 14 },
+        },
+        .{
+            .levels = 5,
+            .expected_order = &.{ 0, 1, 3, 7, 15, 16, 8, 17, 18, 4, 9, 19, 20, 10, 21, 22, 2, 5, 11, 23, 24, 12, 25, 26, 6, 13, 27, 28, 14, 29, 30 },
         },
     }) |data| {
-        const Tree = StaticBinaryTree(u8, data.level);
+        const Tree = StaticBinaryTree(u8, data.levels);
         var tree = Tree.init(0);
 
         {
@@ -130,7 +211,50 @@ test "preorderIterator" {
             var i: usize = 0;
             var it = tree.preorderIterator();
             while (it.next()) |value| : (i += 1) {
-                try std.testing.expectEqual(data.order[i], value);
+                try std.testing.expectEqual(data.expected_order[i], value);
+            }
+        }
+    }
+}
+
+test "inorderIterator" {
+    inline for ([_]TestData{
+        .{
+            .levels = 1,
+            .expected_order = &.{0},
+        },
+        .{
+            .levels = 2,
+            .expected_order = &.{ 1, 0, 2 },
+        },
+        .{
+            .levels = 3,
+            .expected_order = &.{ 3, 1, 4, 0, 5, 2, 6 },
+        },
+        .{
+            .levels = 4,
+            .expected_order = &.{ 7, 3, 8, 1, 9, 4, 10, 0, 11, 5, 12, 2, 13, 6, 14 },
+        },
+        .{
+            .levels = 5,
+            .expected_order = &.{ 15, 7, 16, 3, 17, 8, 18, 1, 19, 9, 20, 4, 21, 10, 22, 0, 23, 11, 24, 5, 25, 12, 26, 2, 27, 13, 28, 6, 29, 14, 30 },
+        },
+    }) |data| {
+        const Tree = StaticBinaryTree(u8, data.levels);
+        var tree = Tree.init(0);
+
+        {
+            var i: usize = 0;
+            while (i < Tree.size) : (i += 1) {
+                tree.items[i] = @intCast(u8, i);
+            }
+        }
+
+        {
+            var i: usize = 0;
+            var it = tree.inorderIterator();
+            while (it.next()) |value| : (i += 1) {
+                try std.testing.expectEqual(data.expected_order[i], value);
             }
         }
     }
